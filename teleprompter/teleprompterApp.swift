f6f37c5aa3d2329c14a5f81db74b0b871d567a94
Timothy Li <timmyli3481@gmail.com>
2024-11-28 12:45:54 +0800

    Initial Commit

---
+++ teleprompter/teleprompterApp.swift
@@ -0,0 +1,17 @@
+//
+//  teleprompterApp.swift
+//  teleprompter
+//
+//  Created by Timmy Li on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct teleprompterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ teleprompterTests/teleprompterTests.swift
@@ -0,0 +1,16 @@
+//
+//  teleprompterTests.swift
+//  teleprompterTests
+//
+//  Created by Timmy Li on 11/28/24.
+//
+
+import Testing
+
+struct teleprompterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
